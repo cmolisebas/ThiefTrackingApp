a9cd06c819052c6ff0f4452a13f45a672a3b2183
Sebastian <smolcastano@gmail.com>
2019-11-13 20:18:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var app = require('express')(); // Create an instance of an Express app
-  mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-//mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings
-//mobileApp.tables.add('Users');
+mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings
+mobileApp.tables.add('Users');
 
-//app.use(mobileApp);
+app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
