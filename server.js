f2652cf76693319c281e3702f0504e861da8b0cb
Sebastian <smolcastano@gmail.com>
2019-11-14 19:24:16 -0500

    server update

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var app = require('express')(); // Create an instance of an Express app
-var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+var mobileApp = mobileApp = require('azure-mobile-apps')({
+    data: {
+        provider: 'mssql',
+        server: 'cmolisebas.database.windows.net',
+        database: 'thieftracking_db',
+        user: 'cmolisebas',
+        password: 's3b4st14n.2019'
+    }
+}); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings
 mobileApp.tables.add('Users');
